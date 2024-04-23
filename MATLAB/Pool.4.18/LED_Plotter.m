b81b17f8114f09b95252e6acb5f8bccd63b96d5d
Jordan Carlin <jordanmcarlin@gmail.com>
2024-04-23 15:07:48 -0700

    Fix pool red resistor

--- MATLAB/Pool.4.18/LED_Plotter.m
+++ MATLAB/Pool.4.18/LED_Plotter.m
@@ -9,7 +9,7 @@ yellowResistance = 2.225e6;
 greenResistance = 1.61e6;
 visibleResistance = 1.431e3;
 IRLEDResistance = 10.03e3;
-redResistance = 218.5e3;
+redResistance = 218.5e4;
 
 %% Setup
 filenum = '033'; % file number for the data you want to read
@@ -100,7 +100,7 @@ IRLEDVoltage = cast(A11, "double")*(3.3/1023);
 redVoltage = cast(A12, "double")*(3.3/1023);
 
 %% Plot voltages
-% subplot(3,1,1);
+subplot(3,1,1);
 hold on
 plot(z, redVoltage, "r*")
 plot(z, yellowVoltage, "*", Color="#EDB120")
@@ -117,56 +117,56 @@ title("Voltage vs Depth", FontSize=20);
 %     fontsize=12);
 axis tight
 
-% %% Convert voltages to currents
-% IRPhotoCurrent = IRPhotoVoltage/IRPhotoResistance;
-% yellowCurrent = yellowVoltage/yellowResistance;
-% greenCurrent = greenVoltage/greenResistance;
-% visibleCurrent = visibleVoltage/visibleResistance;
-% IRLEDCurrent = IRLEDVoltage/IRLEDResistance;
-% redCurrent = redVoltage/redResistance;
+%% Convert voltages to currents
+IRPhotoCurrent = IRPhotoVoltage/IRPhotoResistance;
+yellowCurrent = yellowVoltage/yellowResistance;
+greenCurrent = greenVoltage/greenResistance;
+visibleCurrent = visibleVoltage/visibleResistance;
+IRLEDCurrent = IRLEDVoltage/IRLEDResistance;
+redCurrent = redVoltage/redResistance;
 
-% %% Plot currents
-% subplot(3,1,2);
-% hold on
-% plot(z, redCurrent, "r*")
-% plot(z, yellowCurrent, "*", Color="#EDB120")
-% plot(z, greenCurrent, "g*")
-% plot(z, visibleCurrent, "b*")
-% plot(z, IRLEDCurrent, "c*")
-% plot(z, IRPhotoCurrent, "m*")
-% hold off
-% xlabel("Depth [m]", FontSize=16);
-% ylabel("Current [A]", FontSize=16);
-% title("Current vs Depth", FontSize=20);
-% % legend("Red LED Current", "Yellow LED Current", "Green LED Current", ...
-% %     "Visible Photodiode Current", "IR LED Current", "IR Photodiode Current", ...
-% %     fontsize=12);
-% axis tight
-
-% %% Normalize currents
-% IRPhotoCurrentNorm = IRPhotoCurrent/max(IRPhotoCurrent);
-% yellowCurrentNorm = yellowCurrent/max(yellowCurrent);
-% greenCurrentNorm = greenCurrent/max(greenCurrent);
-% visibleCurrentNorm = visibleCurrent/max(visibleCurrent);
-% IRLEDCurrentNorm = IRLEDCurrent/max(IRLEDCurrent);
-% redCurrentNorm = redCurrent/max(redCurrent);
-
-% %% Plot normalized currents
-% subplot(3,1,3);
-% hold on
-% plot(z, redCurrentNorm, "r*")
-% plot(z, yellowCurrentNorm, "*", Color="#EDB120")
-% plot(z, greenCurrentNorm, "g*")
-% plot(z, visibleCurrentNorm, "b*")
-% plot(z, IRLEDCurrentNorm, "c*")
-% plot(z, IRPhotoCurrentNorm, "m*")
-% hold off
-% xlabel("Depth [m]", FontSize=16);
-% ylabel("Normalized Current Relative to Max", FontSize=16);
-% title("Normalized Current vs Depth", FontSize=20);
+%% Plot currents
+subplot(3,1,2);
+hold on
+plot(z, redCurrent, "r*")
+plot(z, yellowCurrent, "*", Color="#EDB120")
+plot(z, greenCurrent, "g*")
+plot(z, visibleCurrent, "b*")
+plot(z, IRLEDCurrent, "c*")
+plot(z, IRPhotoCurrent, "m*")
+hold off
+xlabel("Depth [m]", FontSize=16);
+ylabel("Current [A]", FontSize=16);
+title("Current vs Depth", FontSize=20);
 % legend("Red LED Current", "Yellow LED Current", "Green LED Current", ...
 %     "Visible Photodiode Current", "IR LED Current", "IR Photodiode Current", ...
-%     'Position',[0.836049968900604 0.857068811310621 0.145490196078431 0.12002567394095], fontsize=12);
-% axis tight
+%     fontsize=12);
+axis tight
+
+%% Normalize currents
+IRPhotoCurrentNorm = IRPhotoCurrent/max(IRPhotoCurrent);
+yellowCurrentNorm = yellowCurrent/max(yellowCurrent);
+greenCurrentNorm = greenCurrent/max(greenCurrent);
+visibleCurrentNorm = visibleCurrent/max(visibleCurrent);
+IRLEDCurrentNorm = IRLEDCurrent/max(IRLEDCurrent);
+redCurrentNorm = redCurrent/max(redCurrent);
+
+%% Plot normalized currents
+subplot(3,1,3);
+hold on
+plot(z, redCurrentNorm, "r*")
+plot(z, yellowCurrentNorm, "*", Color="#EDB120")
+plot(z, greenCurrentNorm, "g*")
+plot(z, visibleCurrentNorm, "b*")
+plot(z, IRLEDCurrentNorm, "c*")
+plot(z, IRPhotoCurrentNorm, "m*")
+hold off
+xlabel("Depth [m]", FontSize=16);
+ylabel("Normalized Current Relative to Max", FontSize=16);
+title("Normalized Current vs Depth", FontSize=20);
+legend("Red LED Current", "Yellow LED Current", "Green LED Current", ...
+    "Visible Photodiode Current", "IR LED Current", "IR Photodiode Current", ...
+    'Position',[0.836049968900604 0.857068811310621 0.145490196078431 0.12002567394095], fontsize=12);
+axis tight
 
 
